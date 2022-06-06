36b41291b1904fc6e30b30b17c0f3ad3321287ed
Mira <5555159m@gmail.com>
2022-06-06 20:53:16 +0300

    add 3 task

--- 03_Copy/Program.cs
+++ 03_Copy/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Написать программу копирования массива
+
+void CopyArr (int[] array)
+{
+    int [] Dolly = new int[array.Length];
+    for(int i = 0; i < array.Length; i++)
+    {
+        Dolly[i] = array[i];
+        Console.Write(Dolly[i] + " ");
+    }
+}
+int [] array = new int [10];
+for(int i = 0; i < 10; i++)
+{
+    array [i] = new Random().Next(1,100);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+CopyArr(array);
